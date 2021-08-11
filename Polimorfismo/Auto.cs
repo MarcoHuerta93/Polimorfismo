7e338760c1b3268570feacc986d0438c8085ecf1
Marco Huerta <marco.huerta.sc@pm.me>
2021-08-11 16:02:19 -0500

    Inicializando Solución

---
+++ Polimorfismo/Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Polimorfismo
+{
+   class Auto
+    {
+        //Propiedades
+        public int HP { get; set; }
+        public string Color { get; set; }
+
+
+        //Constuctor parametrizado
+
+        public Auto(int hp, string color)
+        {
+            this.HP = hp;
+            this.Color = color;
+        }
+
+        //Métodos
+
+        public void MostrarDetalles()
+
+        {
+            Console.WriteLine("HP: {0} - Color: {1}", HP,Color);
+        }
+
+    }
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
